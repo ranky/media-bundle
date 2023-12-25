1d3b1963c5a025d2e7274d1cecaf6d533f7818a4
chiqui3d <nerjacarloscampos@gmail.com>
2023-12-25 11:16:26 +0100

    test: update MediaTwigServiceTest

--- tests/src/Presentation/Twig/MediaTwigServiceTest.php
+++ tests/src/Presentation/Twig/MediaTwigServiceTest.php
@@ -1,4 +1,5 @@
 <?php
+
 declare(strict_types=1);
 
 namespace Ranky\MediaBundle\Tests\Presentation\Twig;
@@ -7,6 +8,8 @@ namespace Ranky\MediaBundle\Tests\Presentation\Twig;
 use Doctrine\Common\Collections\ArrayCollection;
 use Ranky\MediaBundle\Application\DataTransformer\MediaToResponseTransformer;
 use Ranky\MediaBundle\Application\FindMedia\FindMediaByIds;
+use Ranky\MediaBundle\Application\FindMedia\FindMediaByPaths;
+use Ranky\MediaBundle\Application\GetMedia\GetMediaByFilePath;
 use Ranky\MediaBundle\Application\GetMedia\GetMediaById;
 use Ranky\MediaBundle\Domain\Enum\MimeType;
 use Ranky\MediaBundle\Domain\Model\Media;
@@ -32,15 +35,19 @@ class MediaTwigServiceTest extends BaseUnitTestCase
 
     public function testItShouldGetMediaByIdAndConvertMediaToResponse(): void
     {
-        $getMediaById = $this->createMock(GetMediaById::class);
-        $findMediaByIds = $this->createMock(FindMediaByIds::class);
+        $getMediaByPath   = $this->createMock(GetMediaByFilePath::class);
+        $findMediaByPaths = $this->createMock(FindMediaByPaths::class);
+        $getMediaById     = $this->createMock(GetMediaById::class);
+        $findMediaByIds   = $this->createMock(FindMediaByIds::class);
 
         $this->mediaTwigService = new MediaTwigService(
+            $getMediaByPath,
+            $findMediaByPaths,
             $getMediaById,
             $findMediaByIds,
             $this->responseTransformer
         );
-        $mediaResponse = $this->mediaTwigService->mediaToResponse($this->media);
+        $mediaResponse          = $this->mediaTwigService->mediaToResponse($this->media);
         $getMediaById
             ->expects($this->once())
             ->method('__invoke')
@@ -56,15 +63,19 @@ class MediaTwigServiceTest extends BaseUnitTestCase
 
     public function testItShouldFindMediaByIdsAndConvertMediaToArrayResponse(): void
     {
-        $getMediaById = $this->createMock(GetMediaById::class);
-        $findMediaByIds = $this->createMock(FindMediaByIds::class);
+        $getMediaByPath   = $this->createMock(GetMediaByFilePath::class);
+        $findMediaByPaths = $this->createMock(FindMediaByPaths::class);
+        $getMediaById     = $this->createMock(GetMediaById::class);
+        $findMediaByIds   = $this->createMock(FindMediaByIds::class);
 
         $this->mediaTwigService = new MediaTwigService(
+            $getMediaByPath,
+            $findMediaByPaths,
             $getMediaById,
             $findMediaByIds,
             $this->responseTransformer
         );
-        $arrayCollection = new ArrayCollection();
+        $arrayCollection        = new ArrayCollection();
         $arrayCollection->add($this->media);
         $mediaResponse = $this->mediaTwigService->mediaCollectionToArrayResponse($arrayCollection);
         $findMediaByIds
