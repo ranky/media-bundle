38b34e546e8cbf78388b0bc6b79b7fb3754697ac
chiqui3d <nerjacarloscampos@gmail.com>
2023-12-25 11:15:40 +0100

    feat: add new option to MediaFileManagerType for using media path instance of id

--- src/Presentation/Form/RankyMediaFileManagerType.php
+++ src/Presentation/Form/RankyMediaFileManagerType.php
@@ -1,4 +1,5 @@
 <?php
+
 declare(strict_types=1);
 
 namespace Ranky\MediaBundle\Presentation\Form;
@@ -8,6 +9,7 @@ use Ranky\MediaBundle\Presentation\Form\DataTransformer\JsonToArrayTransformer;
 use Ranky\MediaBundle\Presentation\Form\DataTransformer\MediaCollectionTransformer;
 use Ranky\MediaBundle\Presentation\Form\DataTransformer\MediaEntityTransformer;
 use Ranky\MediaBundle\Presentation\Form\DataTransformer\MediaIdToStringTransformer;
+use Ranky\MediaBundle\Presentation\Form\DataTransformer\MediaPathTransformer;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\HiddenType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -25,11 +27,21 @@ class RankyMediaFileManagerType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         if ($options['association'] === true) {
+            if ($options['save_path'] === true) {
+                throw new \InvalidArgumentException('The option "save_path" cannot be used with "association"');
+            }
             if ($options['multiple_selection'] === true) {
                 $builder->addModelTransformer(new MediaCollectionTransformer($this->mediaRepository));
             } else {
                 $builder->addModelTransformer(new MediaEntityTransformer($this->mediaRepository));
             }
+        } elseif ($options['save_path'] === true) {
+            $builder->addModelTransformer(
+                new MediaPathTransformer(
+                    $this->mediaRepository,
+                    $options['multiple_selection']
+                )
+            );
         } elseif ($options['multiple_selection'] === true) {
             $builder->addModelTransformer(new JsonToArrayTransformer());
         } else {
@@ -40,14 +52,16 @@ class RankyMediaFileManagerType extends AbstractType
     public function configureOptions(OptionsResolver $resolver): void
     {
         $resolver->setDefaults([
-            'compound' => false,
+            'compound'           => false,
             'multiple_selection' => false,
-            'modal_title' => 'Media File Manager',
-            'association' => false,
-            'api_prefix' => null,
+            'modal_title'        => 'Media File Manager',
+            'association'        => false,
+            'api_prefix'         => null,
+            'save_path'          => false,
         ]);
 
 
+        $resolver->setAllowedTypes('save_path', 'bool');
         $resolver->setAllowedTypes('multiple_selection', 'bool');
         $resolver->setAllowedTypes('association', 'bool');
         $resolver->setAllowedTypes('modal_title', 'string');
@@ -58,9 +72,9 @@ class RankyMediaFileManagerType extends AbstractType
     {
         $view->vars = \array_replace($view->vars, [
             'multiple_selection' => $options['multiple_selection'],
-            'api_prefix' => $options['api_prefix'],
-            'modal_title' => $options['modal_title'],
-            'association' => $options['association'],
+            'api_prefix'         => $options['api_prefix'],
+            'modal_title'        => $options['modal_title'],
+            'association'        => $options['association'],
         ]);
     }
 
