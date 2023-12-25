fe40f7fdd2da9298ad9cda6f30396daf3f399375
chiqui3d <nerjacarloscampos@gmail.com>
2023-12-25 11:15:40 +0100

    feat: add new option to MediaFileManagerType for using media path instance of id

---
+++ src/Presentation/Form/DataTransformer/MediaPathTransformer.php
@@ -0,0 +1,57 @@
+<?php
+
+declare(strict_types=1);
+
+
+namespace Ranky\MediaBundle\Presentation\Form\DataTransformer;
+
+use Ranky\MediaBundle\Domain\Contract\MediaRepositoryInterface;
+use Ranky\MediaBundle\Domain\ValueObject\MediaId;
+use Symfony\Component\Form\DataTransformerInterface;
+
+class MediaPathTransformer implements DataTransformerInterface
+{
+    public function __construct(
+        private readonly MediaRepositoryInterface $mediaRepository,
+        private readonly bool $multipleSelection = false,
+    ) {
+    }
+
+    public function transform(mixed $value): ?string
+    {
+        if (!$value) {
+            return null;
+        }
+        if ($this->multipleSelection) {
+            $medias   = $this->mediaRepository->findByFileNames($value);
+            $mediaIds = \array_map(static fn($media) => $media->id()->asString(), $medias);
+            return \json_encode($mediaIds, \JSON_THROW_ON_ERROR);
+        }
+
+        return $this->mediaRepository->getByFileName($value)->id()->asString();
+    }
+
+    public function reverseTransform(mixed $value): string|array|null
+    {
+        if (null === $value || '' === $value) {
+            return null;
+        }
+
+        try {
+            if ($this->multipleSelection) {
+                $ids    = \json_decode($value, true, 512, \JSON_THROW_ON_ERROR);
+                $medias = $this->mediaRepository->findByIds(
+                    ...\array_map(static fn(string $id) => MediaId::fromString($id), $ids)
+                );
+
+                return \array_map(static fn($media) => $media->file()->path(), $medias);
+            }
+
+            $media = $this->mediaRepository->getById(MediaId::fromString($value));
+
+            return $media->file()->path();
+        } catch (\Throwable) {
+            return '';
+        }
+    }
+}
--- src/Presentation/Twig/MediaTwigExtension.php
+++ src/Presentation/Twig/MediaTwigExtension.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 namespace Ranky\MediaBundle\Presentation\Twig;
 
 use Ranky\MediaBundle\Domain\Contract\FileUrlResolverInterface;
+use Ranky\MediaBundle\Domain\ValueObject\MediaId;
 use Ranky\SharedBundle\Common\FileHelper;
 use Twig\Extension\AbstractExtension;
 use Twig\TwigFilter;
@@ -22,6 +23,7 @@ class MediaTwigExtension extends AbstractExtension
     {
         return [
             new TwigFilter('human_file_size', [FileHelper::class, 'humanFileSize']),
+            new TwigFilter('isMediaId', [$this, 'isMediaId']),
         ];
     }
 
@@ -33,11 +35,30 @@ class MediaTwigExtension extends AbstractExtension
         ];
     }
 
+    public function isMediaId(mixed $value): bool
+    {
+        if (\is_array($value) && !empty($value)) {
+            $value = reset($value);
+        }
+        return $value && (($value instanceof MediaId || MediaId::isValid($value)));
+    }
+
+    /**
+     * @param string $fileName The file name or the file path
+     * @param bool $absolute
+     * @return string
+     */
     public function mediaUrl(string $fileName, bool $absolute = false): string
     {
         return $this->fileUrlResolver->resolve($fileName, $absolute);
     }
 
+    /**
+     * @param string $fileName The file name or the file path
+     * @param string $breakpoint
+     * @param bool $absolute
+     * @return string
+     */
     public function mediaThumbnailUrl(string $fileName, string $breakpoint, bool $absolute = false): string
     {
         return $this->fileUrlResolver->resolveFromBreakpoint($breakpoint, $fileName, $absolute);
