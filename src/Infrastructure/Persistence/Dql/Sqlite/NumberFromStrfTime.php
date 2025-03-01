31a07d512f6217dc3e0ec4f7ed27e632ef84c8b7
Jose Antonio <jguidos@hotmail.com>
2025-03-01 19:11:45 +0100

    Migrates from Lexer to TokenType

--- src/Infrastructure/Persistence/Dql/Sqlite/NumberFromStrfTime.php
+++ src/Infrastructure/Persistence/Dql/Sqlite/NumberFromStrfTime.php
@@ -7,7 +7,7 @@ namespace Ranky\MediaBundle\Infrastructure\Persistence\Dql\Sqlite;
 
 use Doctrine\ORM\Query\AST\Functions\FunctionNode;
 use Doctrine\ORM\Query\AST\Node;
-use Doctrine\ORM\Query\Lexer;
+use Doctrine\ORM\Query\TokenType;
 use Doctrine\ORM\Query\Parser;
 use Doctrine\ORM\Query\SqlWalker;
 
@@ -30,12 +30,12 @@ abstract class NumberFromStrfTime extends FunctionNode
 
     public function parse(Parser $parser): void
     {
-        $parser->match(Lexer::T_IDENTIFIER);
-        $parser->match(Lexer::T_OPEN_PARENTHESIS);
+        $parser->match(TokenType::T_IDENTIFIER);
+        $parser->match(TokenType::T_OPEN_PARENTHESIS);
 
         $this->date = $parser->ArithmeticPrimary();
 
-        $parser->match(Lexer::T_CLOSE_PARENTHESIS);
+        $parser->match(TokenType::T_CLOSE_PARENTHESIS);
     }
 
     abstract protected function getFormat(): string;
