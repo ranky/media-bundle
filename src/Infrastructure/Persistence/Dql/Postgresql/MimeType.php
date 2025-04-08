97cf816aaaa7deb5b54d007ccb9765a418d69f3d
Jose Antonio <jguidos@hotmail.com>
2025-04-09 01:15:05 +0200

    Adds compatibility with Doctrine ORM ^2.6 || ^3.0

--- src/Infrastructure/Persistence/Dql/Postgresql/MimeType.php
+++ src/Infrastructure/Persistence/Dql/Postgresql/MimeType.php
@@ -5,9 +5,9 @@ namespace Ranky\MediaBundle\Infrastructure\Persistence\Dql\Postgresql;
 
 use Doctrine\ORM\Query\AST\Functions\FunctionNode;
 use Doctrine\ORM\Query\AST\Node;
-use Doctrine\ORM\Query\TokenType;
 use Doctrine\ORM\Query\Parser;
 use Doctrine\ORM\Query\SqlWalker;
+use function class_exists;
 
 
 class MimeType extends FunctionNode
@@ -19,12 +19,17 @@ class MimeType extends FunctionNode
      */
     public function parse(Parser $parser): void
     {
-        $parser->match(TokenType::T_IDENTIFIER);
-        $parser->match(TokenType::T_OPEN_PARENTHESIS);
+        if (!class_exists('Doctrine\ORM\Query\TokenType')) {
+            $tokenType = 'Doctrine\ORM\Query\Lexer';
+        } else {
+            $tokenType = 'Doctrine\ORM\Query\TokenType';
+        }
+        $parser->match($tokenType::T_IDENTIFIER);
+        $parser->match($tokenType::T_OPEN_PARENTHESIS);
 
         $this->field = $parser->ArithmeticPrimary();
 
-        $parser->match(TokenType::T_CLOSE_PARENTHESIS);
+        $parser->match($tokenType::T_CLOSE_PARENTHESIS);
     }
 
 
